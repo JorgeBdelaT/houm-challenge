3b5145630bc301135e423b4251a0f6b8fc0cf8b6
jorge <jabecerra@uc.cl>
2022-01-09 20:30:42 -0300

    fix(infinite-scroll): loading icon position

--- components/infinite-scroller/styles.ts
+++ components/infinite-scroller/styles.ts
@@ -2,11 +2,9 @@ const styles = {
   loadingBox: {
     alignItems: "center",
     display: "flex",
-    height: "100px",
     justifyContent: "center",
     margin: "30px",
     py: 3,
-    width: "100%",
   },
   loadingIcon: (loading: boolean) => ({ display: loading ? "block" : "none" }),
 };
